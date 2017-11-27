b53f2fe86514d32504423bb62d3b51e488dbf08b
kbaumst2 <33259283+kbaumst2@users.noreply.github.com>
2017-11-27 11:02:23 -0500

    added sprite initialization

--- wall1.py
+++ wall1.py
@@ -2,6 +2,7 @@ import pygame
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x, y, img_file):
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("assets/" + img_file).convert()
         self.rect = self.image.get_rect()
         self.rect.x = x
