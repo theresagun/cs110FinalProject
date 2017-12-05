d88c5a0c1633994c5ab8a78f5f2f9d251fd9f30c
theresagun <33259276+theresagun@users.noreply.github.com>
2017-12-05 01:15:35 -0500

    Updated testing file

--- test.py
+++ test.py
@@ -1,74 +1,49 @@
+import pygame
+import main
+
 def main():
 ##########################################################################
     print('### Testing pacman model ###')
     testPacman = pacman.Pacman()
 
-    print('# Test forward #')
-    testPacman.forward(5)
-    assert testPacman.getCoordinates() == (5,0)
+    print('# Test up #')
+    testPacman.turnUp()
+    assert testPacman.direction == 1
 
     print('# Test right #')
-<<<<<<< HEAD
-    testPacman.right(90)
-    testPacman.forward(10)
-    assert testPacman.getCoordinates()== (0,-10)
+    testPacman.turnRight()
+    assert testPacman.direction == 0
 
     print('# Test left #')
-    testPacman.right(-90)
-    testPacman.forward(10)
-    assert testPacman.getCoordinates()== (0,10)
-=======
-    testPacman.right(60)
-    assert testPacman.direction() == 60
+    testPacman.turnLeft()
+    assert testPacman.direction == 2
 
-    print('# Test left #')
-    testPacman.left(60)
-    assert testPacman.direction() == 300
+    print('# Test down #')
+    testPacman.turnDown()
+    assert testPacman.direction == 3
 
-    print('# Test eat #')
-    testPacman.eat()
-    assert testPacman.getCoordinates() == dot.getCoordinates()
->>>>>>> ca24637fd81c8fbcc1c83f46a0c20884537886e9
 ##########################################################################
     print('### Testing ghost model ###')
-    testGhost = ghost.Ghost()
+    testGhost = ghosts.Ghost()
 
-    print('# Test forward #')
-    testGhost.forward(5)
-    assert testGhost.getCoordinates() == (5,0)
+    print('# Test up #')
+    testGhost.turnUp()
+    assert testGhost.direction == 1
 
     print('# Test right #')
-<<<<<<< HEAD
-    testGhost.right(90)
-    testGhost.forward(10)
-    assert testGhost.getCoordinates()== (0,-10)
-
-
-  
-  print('# Test left #')
-    testGhost.right(-90)
-    testGhost.forward(10)
-    assert testGhost.getCoordinates()== (0,10)
-
-    print('# Test kill #')
-    testGhost.kill()
-    assert testGhost.getCoordinates() == testPacman.getCoordinates()
-
-    print(‘# Test Reset #’)
-    testGhost.reset()
-    assert testGhost.exist()
-=======
-    testGhost.right(60)
-    assert testGhost.direction() == 60
+    testGhost.turnRight()
+    assert testGhost.direction == 0
 
     print('# Test left #')
-    testGhost.left(60)
-    assert testGhost.direction() == 300
+    testGhost.turnLeft()
+    assert testGhost.direction == 2
 
-    print('# Test eat #')
-    testGhost.eat()
-    assert testGhost.getCoordinates() == testPacman.getCoordinates()
->>>>>>> ca24637fd81c8fbcc1c83f46a0c20884537886e9
+    print('# Test down #')
+    testGhost.turnDown()
+    assert testGhost.direction == 3
+
+##########################################################################
+'COPY & PASTE TESTING GHOST MODEL FOR DIFFERENT COLOR GHOSTS'
 ##########################################################################
     print('### Testing dot model ###')
     testDot = dot.Dot()
@@ -78,28 +53,7 @@ def main():
     assert testDot.exist() == False
 ##########################################################################
     print('### Testing map model ###')
-    testMap = map.Map()
+    testMap = maps.Map()
     assert testMap.exist() == True
-<<<<<<< HEAD
-
-    print(‘# Testing Collide #’)
-    testmap.checkCollide()
-    assert testmap.getCoordinates() == testPacman.getCoordinates() 
-=======
->>>>>>> ca24637fd81c8fbcc1c83f46a0c20884537886e9
 ##########################################################################
-    print('### Testing score model ###')
-    testScore = score.Score()
-
-    print('# Test current score #')
-    testScore.currentScore(500)
-    assert testScore.displayScore() == 500
-
-    print('# Test high score #')
-    testScore.highScore(5000)
-    assert testScore.displayHighScore == 5000
-
-    print('# Test number of lives #')
-    testScore.loseLife(1)
-    assert testScore.lives == lives - 1
 main()
