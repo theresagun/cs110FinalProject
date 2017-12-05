1194d68a182dafa8929d44825e9edac3c62c97af
kbaumst2 <33259283+kbaumst2@users.noreply.github.com>
2017-12-05 13:34:42 -0500

    high score changed to use json file

--- test.py
+++ test.py
@@ -1,49 +1,74 @@
-import pygame
-import main
-
 def main():
 ##########################################################################
     print('### Testing pacman model ###')
     testPacman = pacman.Pacman()
 
-    print('# Test up #')
-    testPacman.turnUp()
-    assert testPacman.direction == 1
+    print('# Test forward #')
+    testPacman.forward(5)
+    assert testPacman.getCoordinates() == (5,0)
 
     print('# Test right #')
-    testPacman.turnRight()
-    assert testPacman.direction == 0
+<<<<<<< HEAD
+    testPacman.right(90)
+    testPacman.forward(10)
+    assert testPacman.getCoordinates()== (0,-10)
 
     print('# Test left #')
-    testPacman.turnLeft()
-    assert testPacman.direction == 2
+    testPacman.right(-90)
+    testPacman.forward(10)
+    assert testPacman.getCoordinates()== (0,10)
+=======
+    testPacman.right(60)
+    assert testPacman.direction() == 60
 
-    print('# Test down #')
-    testPacman.turnDown()
-    assert testPacman.direction == 3
+    print('# Test left #')
+    testPacman.left(60)
+    assert testPacman.direction() == 300
 
+    print('# Test eat #')
+    testPacman.eat()
+    assert testPacman.getCoordinates() == dot.getCoordinates()
+>>>>>>> ca24637fd81c8fbcc1c83f46a0c20884537886e9
 ##########################################################################
     print('### Testing ghost model ###')
-    testGhost = ghosts.Ghost()
+    testGhost = ghost.Ghost()
 
-    print('# Test up #')
-    testGhost.turnUp()
-    assert testGhost.direction == 1
+    print('# Test forward #')
+    testGhost.forward(5)
+    assert testGhost.getCoordinates() == (5,0)
 
     print('# Test right #')
-    testGhost.turnRight()
-    assert testGhost.direction == 0
+<<<<<<< HEAD
+    testGhost.right(90)
+    testGhost.forward(10)
+    assert testGhost.getCoordinates()== (0,-10)
+
+
+  
+  print('# Test left #')
+    testGhost.right(-90)
+    testGhost.forward(10)
+    assert testGhost.getCoordinates()== (0,10)
+
+    print('# Test kill #')
+    testGhost.kill()
+    assert testGhost.getCoordinates() == testPacman.getCoordinates()
+
+    print(‘# Test Reset #’)
+    testGhost.reset()
+    assert testGhost.exist()
+=======
+    testGhost.right(60)
+    assert testGhost.direction() == 60
 
     print('# Test left #')
-    testGhost.turnLeft()
-    assert testGhost.direction == 2
+    testGhost.left(60)
+    assert testGhost.direction() == 300
 
-    print('# Test down #')
-    testGhost.turnDown()
-    assert testGhost.direction == 3
-
-##########################################################################
-'COPY & PASTE TESTING GHOST MODEL FOR DIFFERENT COLOR GHOSTS'
+    print('# Test eat #')
+    testGhost.eat()
+    assert testGhost.getCoordinates() == testPacman.getCoordinates()
+>>>>>>> ca24637fd81c8fbcc1c83f46a0c20884537886e9
 ##########################################################################
     print('### Testing dot model ###')
     testDot = dot.Dot()
@@ -53,7 +78,28 @@ def main():
     assert testDot.exist() == False
 ##########################################################################
     print('### Testing map model ###')
-    testMap = maps.Map()
+    testMap = map.Map()
     assert testMap.exist() == True
+<<<<<<< HEAD
+
+    print(‘# Testing Collide #’)
+    testmap.checkCollide()
+    assert testmap.getCoordinates() == testPacman.getCoordinates() 
+=======
+>>>>>>> ca24637fd81c8fbcc1c83f46a0c20884537886e9
 ##########################################################################
+    print('### Testing score model ###')
+    testScore = score.Score()
+
+    print('# Test current score #')
+    testScore.currentScore(500)
+    assert testScore.displayScore() == 500
+
+    print('# Test high score #')
+    testScore.highScore(5000)
+    assert testScore.displayHighScore == 5000
+
+    print('# Test number of lives #')
+    testScore.loseLife(1)
+    assert testScore.lives == lives - 1
 main()
