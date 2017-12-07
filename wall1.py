75ba02182628a6039bea18e6c9ea85f161677aec
theresagun <33259276+theresagun@users.noreply.github.com>
2017-12-06 19:31:54 -0500

    doc string

--- wall1.py
+++ wall1.py
@@ -2,6 +2,9 @@ import pygame
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x, y, img_file):
+        '''
+        Initializes wall object with attributes: image, rect, rect.x, rect.y.
+        '''
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("assets/" + img_file).convert()
         self.rect = self.image.get_rect()
