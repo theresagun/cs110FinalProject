7559244be7cc5f3ad4ed358657409a116be476a3
theresagun <33259276+theresagun@users.noreply.github.com>
2017-12-06 19:40:58 -0500

    doc strings galore

--- pacman.py
+++ pacman.py
@@ -3,6 +3,9 @@ import wall1
 
 class Pacman(pygame.sprite.Sprite):
     def __init__(self, x, y, img_file, direction):
+        '''
+        Initializes Pacman object with attributes: image, rect, rect.x, rect.y, speed, lives, direction.
+        '''
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("assets/" + img_file).convert_alpha()
         self.image = pygame.transform.scale(self.image, (15,15)).convert_alpha()
@@ -25,6 +28,9 @@ class Pacman(pygame.sprite.Sprite):
         self.direction = direction
 
     def chooseImg(self):
+        '''
+        Determines what image to represent Pacman character with based on which mode is chosen in main menu.
+        '''
         if self.choose_list==1:
             self.rotated_list=["assets/pacman-right.png","assets/pacman-up.png","assets/pacman-left.png", "assets/pacman-down.png"]
         else:
@@ -32,6 +38,9 @@ class Pacman(pygame.sprite.Sprite):
 
     
     def canMove(self, walls):
+        '''
+        Determines if Pacman character can move based on its location relative to the walls.
+        '''
         #walls (list) - A group of all the wall sprites
         #loops through all the sprites in the walls list and if pacman collides with any of them
         #the function returns False because it means pacman cannot move
@@ -46,6 +55,9 @@ class Pacman(pygame.sprite.Sprite):
         return True
     
     def nodeCollide(self, nodes):
+        '''
+        Determines if Pacman character reaches an intersection.
+        '''
         for node in nodes:
             if self.rect.centerx in range(node.rect.centerx - 5, node.rect.centerx + 5) and self.rect.centery in range(node.rect.centery - 5, node.rect.centery + 5):
                 return (True, node)
@@ -53,24 +65,36 @@ class Pacman(pygame.sprite.Sprite):
         return (False, None)
 
     def dotCollide(self, dots):
+        '''
+        Determines if Pacman character intersects with a dot.
+        '''
         for dot in dots:
             if self.rect.centerx in range(dot.rect.centerx - 5, dot.rect.centerx + 5) and self.rect.centery in range(dot.rect.centery - 5, dot.rect.centery + 5):
                 return (True, dot)
         return (False, None)
     
     def bigDotCollide(self, dots):
+        '''
+        Determines if Pacman character intersects with a big dot.
+        '''
         for dot in dots:
            if self.rect.centerx in range(dot.rect.centerx - 5, dot.rect.centerx + 5) and self.rect.centery in range(dot.rect.centery - 5, dot.rect.centery + 5):
                 return (True, dot)
         return (False, None)
 
     def ghostCollide(self, ghosts):
+        '''
+        Determines if Pacman character intersects with a ghost.
+        '''
         for ghost in ghosts:
            if self.rect.centerx in range(ghost.rect.centerx - 5, ghost.rect.centerx + 5) and self.rect.centery in range(ghost.rect.centery - 5, ghost.rect.centery + 5):
                 return (True, ghost)
         return (False, None)
  
     def outsideMap(self):
+        '''
+        When Pacman character leaves the screen on one side, he comes back on the other.
+        '''
         if self.rect.midleft[0] > 420:
             self.rect.x = 1
         elif self.rect.midright[0] < 15:
@@ -78,10 +102,16 @@ class Pacman(pygame.sprite.Sprite):
         
 
     def correctTurn(self, dot):
+        '''
+        Makes Pacman character's position the center of the path when he turns.
+        '''
         self.rect.centerx = dot.rect.centerx
         self.rect.centery = dot.rect.centery
 
     def move(self):
+        '''
+        Moves the Pacman character using a set speed and direction.
+        '''
         #moves the surface by whatever the speed is set to.
         #also when the direction is changed, the image direction should change too
         if self.direction == 0:
@@ -101,22 +131,33 @@ class Pacman(pygame.sprite.Sprite):
 
 
     def turnRight(self):
-        
+        '''
+        Changes Pacman character's direction to right, changes image to face right.
+        '''
         self.direction = 0
         self.rotated_img=pygame.image.load(self.rotated_list[0]).convert_alpha()
        
         #self.image = pygame.image.load("assets/" + img_file + '_right').convert()
     def turnUp(self):
+        '''
+        Changes Pacman character's direction to up, changes image to face up.
+        '''
         self.direction = 1
         self.rotated_img=pygame.image.load(self.rotated_list[1]).convert_alpha()
         
         #self.image = pygame.image.load("assets/" + img_file + '_up').convert()
     def turnLeft(self):
+        '''
+        Changes Pacman character's direction to left, changes image to face left.
+        '''
         self.direction = 2
         self.rotated_img=pygame.image.load(self.rotated_list[2]).convert_alpha()
         
         #self.image = pygame.image.load("assets/" + img_file + '_left').convert()
     def turnDown(self):
+        '''
+        Changes Pacman character's direction to down, changes image to face down.
+        '''
         self.direction = 3
         self.rotated_img=pygame.image.load(self.rotated_list[3]).convert_alpha()     
         
